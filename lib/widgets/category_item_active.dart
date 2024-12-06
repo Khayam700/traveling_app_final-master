c00cf27f4441ef34fa75d4b05bfb9d9d1070b3e5
Khayam700 <khayamalpood700@gamil.com>
2024-12-06 05:43:25 +0300

    جاهز وانا فدالش

---
+++ lib/widgets/category_item_active.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import '../screens/category_trips_screen.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+  final String detail;
+  final String sup_title;
+
+
+  CategoryItem(this.id, this.title, this.imageUrl,this.sup_title ,this.detail);
+
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).pushNamed(
+      CategoryTripsScreen.screenRoute,
+      arguments: {
+        'id': id,
+        'imageUrl': imageUrl,
+        'sup_title': sup_title,
+        'detail': detail,
+
+        'title': title,
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Stack(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: Image.network(
+              imageUrl,
+              height: 250,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            child: Text(
+              title,
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(0.4),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
